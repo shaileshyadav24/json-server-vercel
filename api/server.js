d4c4be5652d2419de1cf728ee4057eabdd226219
shaileshyadav24 <shaileshkyadav24@gmail.com>
2025-04-01 18:18:10 -0400

    Added new json

--- api/server.js
+++ api/server.js
@@ -20,10 +20,9 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log('JSON Server is running')
 })
 
